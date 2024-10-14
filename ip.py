e71693657b6d74f7734d43a5ba9596df7cec281e
Valentin Pundikov <nkbotcman@gmail.com>
2024-10-14 14:41:41 +0300

    full_get_ip_data

--- ip.py
+++ ip.py
@@ -1,9 +1,17 @@
 import requests
+from prettytable import PrettyTable
 
 def get_ip_info(ip_address):
     url = f'http://ip-api.com/json/{ip_address}'
     response = requests.get(url)
     return response.json()
 
-ip_info = get_ip_info('8.8.8.8')
-print(ip_info)
+def print_ip_info_pretty(ip_info):
+    table = PrettyTable()
+    table.field_names = ["Field", "Value"]
+    for key, value in ip_info.items():
+        table.add_row([key, value])
+    print(table)
+
+ip_info = get_ip_info('news.de')
+print_ip_info_pretty(ip_info)
