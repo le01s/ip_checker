314ff18fa9fc7ec3734c785bd24277079a76ed44
Valentin Pundikov <nkbotcman@gmail.com>
2024-10-15 08:08:07 +0300

    Deleting as.py

--- as.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def get_as_info(as_number):
-    url = f'https://stat.ripe.net/data/as-overview/data.json?resource=AS{as_number}'
-    response = requests.get(url)
-    return response.json()
-
-as_info = get_as_info('15169')  # Пример AS 15169 (Google LLC)
-print(as_info)
